80552b8688b4f7612c01de32188daf0538b1d626
Sharanya Thevakumaran <sharanyat@hotmail.com>
2023-06-02 17:30:58 -0400

    updated javascript fetch request

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,31 @@
-var weatherAPIKey = bf19735f435ce22b8b0b5b013236e4cc;
+var weatherAPIKey = 'bf19735f435ce22b8b0b5b013236e4cc';
 
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var city = 'Toronto';
+var locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${weatherAPIKey}`;
 
 
-//fetch -- fetch(queryURL)
+fetch(locationUrl) 
+  .then(function(response) {
+    console.log(response);
+    return response.json();
 
-var url = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+  })
+.then(function(locationData) {
+var lat = locationData[0].lat;
+var lon = locationData[0].lon;
+console.log(lat, lon);
+var weatherURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
+fetch(weatherURL)
+  .then(function(response) {return response.json();})
+  .then(function(weatherData){
+    console.log(weatherData);
+
+  })
+
+
+
+
+});
+
+
+
