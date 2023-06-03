08084b7591e66404137a566e50dd84be9d8fc195
Sharanya Thevakumaran <sharanyat@hotmail.com>
2023-06-03 08:40:15 -0400

    added html content to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
+var searchLocation = document.querySelector('#searchLocation');
+var textBox = document.querySelector('.textBox');
+var searchBtn = document.querySelector('searchBtn');
+var currentWeatherBox = document.querySelector('#currentWeatherBox');
+var currentWeatherHeader = document.querySelector('.currentWeatherHeader');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+var futureWeatherHeader = document.querySelector('#futureWeatherHeader');
+var futureWeatherBox = document.querySelector('#futureWeatherBox');
 var weatherAPIKey = 'bf19735f435ce22b8b0b5b013236e4cc';
-
 var city = 'Toronto';
 var locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${weatherAPIKey}`;
 
@@ -19,7 +28,30 @@ fetch(weatherUrl)
   .then(function(response) {return response.json();})
   .then(function(weatherData){
     console.log(weatherData);
+    temp.textContent = "Temperature: " + weatherData.list.main.temp;
+    console.log(weatherData.list.main);
+    wind.textContent = "Wind: " + weatherData.list.wind.speed;
+    humidity.textContent = 'Humidity: ' + weatherData.list.main.humidity;
+    var currentDate = dayjs().format('MM/DD/YYYY');
+    currentWeatherHeader.textContent = city + currentDate;
+    console.log(currentWeatherHeader);
+    futureWeatherHeader.textContent = '5 Day Forecast:';
+    futureWeatherBox.innerHTML = ''; //reset info
 
+    for (var i = 0; i < 5; i++) {
+      var col = document.createElement('div');
+      col.setAttribute('class', 'col');
+      var fiveDays = document.createElement('div');
+      fiveDays.setAttribute('class', 'fiveDaysBox');
+      var fiveDaysContent = document.createElement('div');
+      fiveDaysContent.setAttribute('class', 'fiveDaysContent');
+      var h3 = document.createElement('h3');
+      h3.textContent = dayjs(weatherData).format('MM/DD/YYYY');
+      var futureTemp = document.createElement('p');
+      futureTemp.textContent = 'Temp: ' + weatherData;
+      var futureWind = document.createElement('p');
+      var futureHumidity = document.createElement('p');
+    }
   })
 
 
